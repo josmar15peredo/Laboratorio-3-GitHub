5e6918742b8ebb50e8edcb4eb34318edc96906d4
oliver <87iver@gmail.com>
2026-03-12 12:38:37 -0400

    Resuelve problema operadores 2

---
+++ web-operador/web3/src/web3/Main.java
@@ -0,0 +1,30 @@
+package web3;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int x;
+		x = sc.nextInt();
+		for (int i = 0; i < x; i++) {
+
+		    int a, b;
+		    a = sc.nextInt();
+		    b = sc.nextInt();
+
+		    if (a > b) {
+		        System.out.println(">");
+		    }
+		    else if (a == b) {
+		        System.out.println("=");
+		    }
+		    else {
+		        System.out.println("<");
+		    }
+
+		}
+
+	}
+
+}
